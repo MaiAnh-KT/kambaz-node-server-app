45601b68832e2aa423569f7d1e00ce8ec5408397
MaiAnh-KT <132906105+MaiAnh-KT@users.noreply.github.com>
2025-11-21 02:18:58 -0500

    to 5.2.4

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+export default function Hello(app) {
+    const sayHello = (req, res) => {
+        res.send("Life is good!");
+    };
+    const sayWelcome = (req, res) => {
+        res.send("Welcome to Full Stack Development!");
+    };
+    app.get("/hello", sayHello);
+    app.get("/", sayWelcome);
+}
