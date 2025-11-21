c5a01e44ac9fb05844f82da8f6239e19769fcd3a
MaiAnh-KT <132906105+MaiAnh-KT@users.noreply.github.com>
2025-11-21 18:32:31 -0500

    to 5.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -30,6 +30,11 @@ let todos = [
         todos.push(newTodo);
         res.json(todos);
     };    
+    const postNewTodo = (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+      };    
     const getTodoById = (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
@@ -53,18 +58,46 @@ let todos = [
         todo.description = description;
         res.json(todos);
     };
+    const updateTodo = (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+            return;
+        }
+        todos = todos.map((t) => {
+          if (t.id === parseInt(id)) {
+            return { ...t, ...req.body };
+          }
+          return t;
+        });
+        res.sendStatus(200);
+    };    
     const removeTodo = (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
         todos.splice(todoIndex, 1);
         res.json(todos);
     };
+    const deleteTodo = (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+        }      
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    };
+    app.delete("/lab5/todos/:id", deleteTodo);
+    app.put("/lab5/todos/:id", updateTodo);
     app.get("/lab5/todos/:id/description/:description", updateTodoDescription);
     app.get("/lab5/todos/:id/completed/:completed", updateTodoCompleted);
     app.get("/lab5/todos/:id/title/:title", updateTodoTitle);
     app.get("/lab5/todos/:id/delete", removeTodo);    
     app.get("/lab5/todos", getTodos);
     app.get("/lab5/todos/create", createNewTodo);
+    app.post("/lab5/todos", postNewTodo);
     app.get("/lab5/todos/:id", getTodoById);
   };
   
