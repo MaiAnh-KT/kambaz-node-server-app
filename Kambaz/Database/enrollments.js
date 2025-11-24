940116d3791ffb8ac53e029809e76ad340cb70f6
MaiAnh-KT <132906105+MaiAnh-KT@users.noreply.github.com>
2025-11-24 00:00:17 -0500

    to 5.3.6

---
+++ Kambaz/Database/enrollments.js
@@ -0,0 +1,72 @@
+export default [
+  { "id": "1", "user": "123", "course": "1234" },
+  { "id": "2", "user": "345", "course": "1234" },
+  { "id": "3", "user": "456", "course": "1234" },
+  { "id": "4", "user": "890", "course": "1234" },
+
+  { "id": "5", "user": "234", "course": "2234" },
+  { "id": "6", "user": "456", "course": "2234" },
+  { "id": "7", "user": "567", "course": "2234" },
+  { "id": "8", "user": "789", "course": "2234" },
+
+  { "id": "9", "user": "123", "course": "4444" },
+  { "id": "10", "user": "234", "course": "4444" },
+  { "id": "11", "user": "345", "course": "4444" },
+  { "id": "12", "user": "890", "course": "4444" },
+
+  { "id": "13", "user": "456", "course": "5678" },
+  { "id": "14", "user": "567", "course": "5678" },
+  { "id": "15", "user": "789", "course": "5678" },
+  { "id": "16", "user": "890", "course": "5678" },
+
+  { "id": "17", "user": "234", "course": "6678" },
+  { "id": "18", "user": "345", "course": "6678" },
+  { "id": "19", "user": "567", "course": "6678" },
+  { "id": "20", "user": "890", "course": "6678" },
+
+  { "id": "21", "user": "123", "course": "7778" },
+  { "id": "22", "user": "345", "course": "7778" },
+  { "id": "23", "user": "678", "course": "7778" },
+  { "id": "24", "user": "789", "course": "7778" },
+
+  { "id": "25", "user": "123", "course": "RS101" },
+  { "id": "26", "user": "456", "course": "RS101" },
+  { "id": "27", "user": "678", "course": "RS101" },
+  { "id": "28", "user": "890", "course": "RS101" },
+
+  { "id": "29", "user": "234", "course": "RS102" },
+  { "id": "30", "user": "567", "course": "RS102" },
+  { "id": "31", "user": "789", "course": "RS102" },
+  { "id": "32", "user": "678", "course": "RS102" },
+
+  { "id": "33", "user": "345", "course": "RS103" },
+  { "id": "34", "user": "456", "course": "RS103" },
+  { "id": "35", "user": "567", "course": "RS103" },
+  { "id": "36", "user": "678", "course": "RS103" },
+
+  { "id": "37", "user": "234", "course": "RS104" },
+  { "id": "38", "user": "456", "course": "RS104" },
+  { "id": "39", "user": "567", "course": "RS104" },
+  { "id": "40", "user": "789", "course": "RS104" },
+
+  { "id": "41", "user": "123", "course": "RS105" },
+  { "id": "42", "user": "345", "course": "RS105" },
+  { "id": "43", "user": "567", "course": "RS105" },
+  { "id": "44", "user": "789", "course": "RS105" },
+
+  { "id": "45", "user": "234", "course": "RS106" },
+  { "id": "46", "user": "345", "course": "RS106" },
+  { "id": "47", "user": "456", "course": "RS106" },
+  { "id": "48", "user": "890", "course": "RS106" },
+
+  { "id": "49", "user": "567", "course": "RS107" },
+  { "id": "50", "user": "678", "course": "RS107" },
+  { "id": "51", "user": "789", "course": "RS107" },
+  { "id": "52", "user": "890", "course": "RS107" },
+
+  { "id": "53", "user": "123", "course": "RS108" },
+  { "id": "54", "user": "234", "course": "RS108" },
+  { "id": "55", "user": "678", "course": "RS108" },
+  { "id": "56", "user": "789", "course": "RS108" }
+];
+  
---
+++ Kambaz/Database/modules.js
@@ -0,0 +1,298 @@
+export default [
+  {
+      "_id": "1234",
+      "name": "React JS",
+      "description": "Basic principles of React JS.",
+      "course": "CS1234",
+      "lessons": [
+        {
+          "id": "L101",
+          "name": "History of React JS",
+          "description": "A brief history of React JS.",
+          "module": "M101"
+        },
+        {
+          "id": "L102",
+          "name": "React JS Fundamentals",
+          "description": "Basic principles of React JS.",
+          "module": "M101"
+        },
+        {
+          "id": "L103",
+          "name": "React JS",
+          "description": "Overview of React JS.",
+          "module": "M101"
+        }
+      ]
+  },
+  {
+      "_id": "2234",
+      "name": "iOS Mobile App Development",
+      "description": "iOS Mobile App Development",
+      "course": "CS2234",
+      "lessons": [
+        {
+          "id": "L201",
+          "name": "History of iOS Development",
+          "description": "A brief history of iOS development.",
+          "module": "M102"
+        },
+        {
+          "id": "L202",
+          "name": "Swift Fundamentals",
+          "description": "Learn the basics of Swift programming.",
+          "module": "M102"
+        },
+        {
+          "id": "L203",
+          "name": "Building a Simple App",
+          "description": "Create your first iOS app with SwiftUI.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "4444",
+      "name": "UIUX Principles",
+      "description": "UIUX Principles",
+      "course": "CS4444",
+      "lessons": [
+        {
+          "id": "L301",
+          "name": "Design Fundamentals",
+          "description": "Introduction to UI/UX design principles.",
+          "module": "M103"
+        },
+        {
+          "id": "L302",
+          "name": "Usability Testing",
+          "description": "Learn about usability and testing methods.",
+          "module": "M103"
+        },
+        {
+          "id": "L303",
+          "name": "Wireframing and Prototyping",
+          "description": "Creating wireframes and prototypes for apps.",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "5678",
+      "name": "Identity and Brand Design",
+      "description": "Identity and Brand Design",
+      "course": "DE5678",
+      "lessons": [
+        {
+          "id": "L401",
+          "name": "Brand Fundamentals",
+          "description": "Introduction to branding principles.",
+          "module": "M104"
+        },
+        {
+          "id": "L402",
+          "name": "Logo Design",
+          "description": "Designing effective logos for brands.",
+          "module": "M104"
+        },
+        {
+          "id": "L403",
+          "name": "Brand Guidelines",
+          "description": "Creating consistent brand identity rules.",
+          "module": "M104"
+        }
+      ]
+    },
+    {
+      "_id": "6678",
+      "name": "Interaction Design",
+      "description": "Interaction Design",
+      "course": "DE6678",
+      "lessons": [
+        {
+          "id": "L501",
+          "name": "Wireframing",
+          "description": "Design wireframes for interactive experiences.",
+          "module": "M105"
+        },
+        {
+          "id": "L502",
+          "name": "Prototyping",
+          "description": "Create interactive prototypes for testing.",
+          "module": "M105"
+        },
+        {
+          "id": "L503",
+          "name": "User Flows",
+          "description": "Design seamless user flows in your projects.",
+          "module": "M105"
+        }
+      ]
+    },
+    {
+      "_id": "7778",
+      "name": "Experience Design",
+      "description": "Experience Design",
+      "course": "DE7778",
+      "lessons": [
+        {
+          "id": "L601",
+          "name": "UX Research",
+          "description": "Conduct research to inform design decisions.",
+          "module": "M106"
+        },
+        {
+          "id": "L602",
+          "name": "Usability Testing",
+          "description": "Test designs with users for feedback.",
+          "module": "M106"
+        },
+        {
+          "id": "L603",
+          "name": "Experience Mapping",
+          "description": "Map user journeys for better experience design.",
+          "module": "M106"
+        }
+      ]
+    },
+    {
+      "_id": "8888",
+      "name": "XR Design",
+      "description": "XR Design",
+      "course": "DE8888",
+      "lessons": [
+        {
+          "id": "L701",
+          "name": "VR Prototyping",
+          "description": "Create virtual reality prototypes.",
+          "module": "M107"
+        },
+        {
+          "id": "L702",
+          "name": "AR Interaction Design",
+          "description": "Design AR interactions for apps.",
+          "module": "M107"
+        },
+        {
+          "id": "L703",
+          "name": "Immersive Experiences",
+          "description": "Design immersive experiences for users.",
+          "module": "M107"
+        }
+      ]
+  },
+  {
+    "_id": "M101",
+    "name": "Introduction to Rocket Propulsion",
+    "description": "Basic principles of rocket propulsion and rocket engines.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "id": "L101",
+        "name": "History of Rocketry",
+        "description": "A brief history of rocketry and space exploration.",
+        "module": "M101"
+      },
+      {
+        "id": "L102",
+        "name": "Rocket Propulsion Fundamentals",
+        "description": "Basic principles of rocket propulsion.",
+        "module": "M101"
+      },
+      {
+        "id": "L103",
+        "name": "Rocket Engine Types",
+        "description": "Overview of different types of rocket engines.",
+        "module": "M101"
+      }
+    ]
+  },
+  {
+    "_id": "M102",
+    "name": "Fuel and Combustion",
+    "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "id": "L201",
+        "name": "Rocket Fuel",
+        "description": "Overview of different types of rocket fuels.",
+        "module": "M102"
+      },
+      {
+        "id": "L202",
+        "name": "Combustion Processes",
+        "description": "Understanding combustion processes and efficiency.",
+        "module": "M102"
+      },
+      {
+        "id": "L203",
+        "name": "Combustion Instability",
+        "description": "Understanding combustion instability and mitigation.",
+        "module": "M102"
+      }
+    ]
+  },
+  {
+    "_id": "M103",
+    "name": "Nozzle Design",
+    "description": "Principles of rocket nozzle design and performance optimization.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "id": "L301",
+        "name": "Nozzle Design",
+        "description": "Overview of different types of rocket nozzles.",
+        "module": "M103"
+      },
+      {
+        "id": "L302",
+        "name": "Nozzle Performance",
+        "description": "Understanding nozzle performance and efficiency.",
+        "module": "M103"
+      },
+      {
+        "id": "L303",
+        "name": "Nozzle Optimization",
+        "description": "Optimizing nozzle design for specific applications.",
+        "module": "M103"
+      }
+    ]
+  },
+  {
+    "_id": "M201",
+    "name": "Fundamentals of Aerodynamics",
+    "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M202",
+    "name": "Subsonic and Supersonic Flow",
+    "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M203",
+    "name": "Aerodynamic Heating",
+    "description": "Study of aerodynamic heating and thermal protection systems.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M301",
+    "name": "Spacecraft Structural Design",
+    "description": "Fundamentals of designing spacecraft structures and materials selection.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M302",
+    "name": "Orbital Mechanics",
+    "description": "Understanding orbital dynamics and mission planning.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M303",
+    "name": "Spacecraft Systems Engineering",
+    "description": "Overview of spacecraft systems and subsystems engineering.",
+    "course": "RS103"
+  }
+];
